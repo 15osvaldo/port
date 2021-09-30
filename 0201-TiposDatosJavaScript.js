3048b27d68a82114e539fe04fa3c32a4bfbaf0fe
15osvaldo <60802835+15osvaldo@users.noreply.github.com>
2021-09-30 06:15:29 -0600

    añadi archivos

---
+++ 0201-TiposDatosJavaScript.js
@@ -0,0 +1,12 @@
+var nombre = 'juan';
+var apellido = 'Perez';
+
+ var nombreCompleto = nombre + ' ' + apellido;
+ console.log(nombreCompleto);
+
+ var nombreCompleto2 = 'Carlo'+ ' ' + 'Lara';
+ console.log(nombreCompleto2);
+
+ var x = nombre + 2 + 4;
+ console.log(x);
+
---
+++ 03-01SintaxisBasicaVariable.js
@@ -0,0 +1,14 @@
+//notscion de altas y bajas
+let nombreCompleto = 'juan Perez';
+console.log( nombreCompleto );
+
+let x, y;
+x= 10, y= 20;
+let z = x + y;
+console.log(z);
+
+let nombreCompleto2 = 'Juan Perez';
+let NombreCompleto = ' Carlos Lara';
+
+console.log(nombreCompleto2);
+console.log(NombreCompleto);
